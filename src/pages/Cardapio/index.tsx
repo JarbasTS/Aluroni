4898a2886cf7649cb1c1138bf48d4702396ff41b
JarbasTS <jarbastavares7@gmail.com>
2023-06-23 21:13:08 -0300

    :sparkles: conclusão do curso

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -4,6 +4,7 @@ import styles from './Cardapio.module.scss';
 import logo from 'assets/logo.svg';
 import Filtros from './Filtros';
 import Ordenador from './Ordenador';
+import Itens from './Itens';
 
 export default function Cardapio() {
   const [busca, setBusca] = useState('');
@@ -25,6 +26,7 @@ export default function Cardapio() {
           <Filtros filtro={filtro} setFiltro={setFiltro} />
           <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
         </div>
+        <Itens busca={busca} filtro={filtro} ordenador={ordenador} />
       </section>
     </main>
   );
